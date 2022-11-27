c95a739cf164a56e263dd0e198ee83fd788769ab
Daniel <danielchou777@gmail.com>
2022-11-27 11:43:42 +0800

    Add level 3 authentication - env vars

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
 
 const app = express();
+
+console.log(process.env.API_KEY)
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -17,8 +20,7 @@ const userSchema = new mongoose.Schema({
 	password: String,
 });
 
-const secret = 'Thisisourlittlesecret.';
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model('User', userSchema);
 
